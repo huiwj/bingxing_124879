c34eae3249c66a5801c0ff5fa35a376c313d7bb4
huiwj <154410046+huiwj@users.noreply.github.com>
2024-04-24 20:51:45 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,103 +1,101 @@
+
 #include<iostream>
-#include<windows.h>
-#include<stdlib.h>
+#include<ctime>
+#include<cstdlib>
+#include<vector>
+#include <algorithm>  
+#include <random> 
+#include <numeric>
 #include<chrono>
 using namespace std;
- long long int** b;
-void init(int n)
-{
 
-	b = new long long int* [n];
+void initial_matrix(int n,std::vector<std::vector<float>>& a)
+{
+	// 填充上三角矩阵
+	srand((unsigned)time(0));
+	for(int i=0;i<n;i++)
+		for (int j=i;j<n;j++)
+		{
+			a[i][j] = float(1+rand()%50);
+		}
+	// 复制下三角元素以保持矩阵对称性  
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			a[i][j] = a[j][i];
+		}
+	}
+	// 随机交换行  
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+	std::vector<int> indices(n);
+	std::iota(indices.begin(), indices.end(), 0); // 填充索引向量  
+	std::shuffle(indices.begin(), indices.end(), generator); // 打乱索引  
+
+	// 使用打乱后的索引交换行  
+	std::vector<std::vector<float>> temp(n, std::vector<float>(n)); // 临时矩阵  
+	for (int i = 0; i < n; ++i) {
+		temp[i] = a[indices[i]];
+	}
+	a = temp; // 将临时矩阵赋值给原矩阵  
+	return;
+}
+
+void initial_VECT(int n, std::vector<float>& b)
+{
+	srand((unsigned)time(0));
 	for (int i = 0; i < n; i++)
 	{
-		*(b + i) = new long long int[n];
+		b[i] = float(2 + rand()%50);
 	}
+}
+
+void Gaussian_Elimination(int n, std::vector<std::vector<float>>& a, std::vector<float>& b, std::vector<float>& x)
+{
+	//消去
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			b[i][j] = 2*i + j + 1;
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			float factor = a[j][i] / a[i][i];
+			for (int h = i ; h < n; h++)
+			{
+				a[j][h] = a[j][h] - factor * a[i][h];
+			}
+			b[j] = b[j] - factor * b[i];
+		}
+	}
+	//回代
+	x[n-1] = b[n-1] / a[n-1][n-1];
+	for (int i = n - 2; i >= 0; i--)
+	{
+		float sum = b[i];
+		for (int j = i + 1; j < n; j++)
+		{
+			sum = sum - a[i][j] * x[j];
+		}
+		x[i] = sum / a[i][i];
+	}
+	
 }
 int main()
 {
-	long long head, tail, freq;
-	int n;
-	cin >> n;
-	int*a;
-	a = new int[n];
-	for (int i = 0; i < n; i++)
-		a[i]=i+1;
-	init(n);
-	long long int* sum;
-	sum = new long long int[n];
-	auto beforeTime1 = std::chrono::steady_clock::now();
-	for (int i = 0; i < n; i++)
-	{
-		sum[i] = 0;
-		for (int j = 0; j < n; j++)
+	int N = 20;
+		for (int k = 0; k < 3; k++)
 		{
-			sum[i] += b[j][i] * a[j];
+			std::vector<std::vector<float>> a(N, vector<float>(N));
+			std::vector<float> b(N);
+			std::vector<float> x(N);
+			initial_matrix(N, a);
+			initial_VECT(N, b);
+			auto start = std::chrono::high_resolution_clock::now();
+			Gaussian_Elimination(N, a, b, x);
+			auto end = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> diff = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+			std::cout << "Execution time: " << diff.count() << "nanoseconds." << std::endl;
+			for (int i = 0; i < N; i++)
+			{
+				cout << x[i] << " ";
+			}
 		}
-	}
-	auto afterTime1 = std::chrono::steady_clock::now();
-	double duration_second1 = std::chrono::duration<double,std::nano>(afterTime1 - beforeTime1).count();
-	cout << "耗时：" << duration_second1 <<"纳秒" << endl;
-	/*
-	cout << "结果为：" << endl;
-	for (int i = 0; i < n; i++)
-	{
-		cout << sum[i]<<" ";
-	}
-	cout << endl;
-	*/
-	auto beforeTime = std::chrono::steady_clock::now();
-	for (int i = 0; i < n; i++)
-		sum[i] = 0;
-	for (int j = 0; j < n; j++)
-		for (int i = 0; i < n; i++)
-			sum[i] += b[j][i] * a[j];
-	auto afterTime = std::chrono::steady_clock::now();
-	double duration_second = std::chrono::duration<double, std::nano>(afterTime - beforeTime).count();
-	cout << "耗时：" << duration_second << "纳秒" << endl;
-	
-	/*
-	cout << "结果为：" << endl;;
-	for (int i = 0; i < n; i++)
-	{
-		cout << sum[i] << " ";
-	}
-	cout << endl;
-	*/
-	//求和
-	int s = 0;
-	auto beforeTime2 = std::chrono::steady_clock::now();
-	for (int i = 0; i < n; i++)
-		s += a[i];
-	auto afterTime2 = std::chrono::steady_clock::now();
-	double duration_second2 = std::chrono::duration<double, std::nano>(afterTime2 - beforeTime2).count();
-	cout << "耗时：" << duration_second2 << "纳秒" << endl;
-	cout << "结果：" << s << endl;
-
-	auto beforeTime4 = std::chrono::steady_clock::now();
-	long long int s1 = 0, s2 = 0;
-	for (int i = 0; i < n; i = i + 2)
-	{
-		s1 += a[i];
-		s2 += a[i + 1];
-	}
-	s1 = s1 + s2;
-	auto afterTime4 = std::chrono::steady_clock::now();
-	double duration_second4 = std::chrono::duration<double, std::nano>(afterTime4 - beforeTime4).count();
-	cout << "耗时：" << duration_second4 << "纳秒" << endl;
-	cout << "结果：" << s1 << endl;
-
-	auto beforeTime3 = std::chrono::steady_clock::now();
-	for (int m = n; m > 1; m /= 2)
-		for (int i = 0; i < m / 2; i++)
-			a[i] = a[i * 2] + a[i * 2 + 1];
-	auto afterTime3 = std::chrono::steady_clock::now();
-	double duration_second3 = std::chrono::duration<double, std::nano>(afterTime3 - beforeTime3).count();
-	cout << "耗时：" << duration_second3 << "纳秒" << endl;
-	cout << "结果：" << a[0] << endl;
-
-	
 	return 0;
 }
